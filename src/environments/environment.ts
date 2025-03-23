b8bd40b8aed687f101b3926ceef661e4de86b53b
hhfrancois <francois.achache@gmail.com>
2025-03-23 05:21:58 +0100

    Update .gitignore to exclude .env and .env.bak files; modify environment.ts for local Supabase configuration with updated URL and anonKey.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   supabase: {
-    url: 'VOTRE_URL_SUPABASE',
-    anonKey: 'VOTRE_CLE_PUBLIQUE'
+    url: 'http://localhost:54321',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
   },
   stripe: {
     publicKey: 'pk_test_51R0h2VLJCrbo8n9zhGPY9DIVhwKrlLFzUQA8AhavloPHtavV2cVfoTbk1qRRD0R7SwYV5wyfE3jyCKhT9WyUAuLy00ax47FDgu',
