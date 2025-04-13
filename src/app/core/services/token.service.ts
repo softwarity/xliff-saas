4ec80cc766920fcaa66e07dd4017921e232e9c00
hhfrancois <francois.achache@gmail.com>
2025-04-13 11:26:36 +0200

    Integrate ToastService for error handling in TokenService
    - Added ToastService to TokenService to provide user-friendly notifications for error scenarios when storing, retrieving, and removing tokens.
    - Implemented toast notifications for errors related to token operations, enhancing user feedback during authentication processes.
    - Refactored error handling to replace direct error logging with toast messages for a better user experience.

--- src/app/core/services/token.service.ts
+++ src/app/core/services/token.service.ts
@@ -3,6 +3,7 @@ import { catchError, from, map, Observable, of } from 'rxjs';
 import { ProviderType } from '../../shared/models/provider-type';
 import { AuthService } from './auth.service';
 import { SupabaseClientService } from './supabase-client.service';
+import { ToastService } from './toast.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,28 +11,37 @@ import { SupabaseClientService } from './supabase-client.service';
 export class TokenService {
   private auth = inject(AuthService);
   private supabase = inject(SupabaseClientService);
-
+  private toastService = inject(ToastService);
 
   storeToken(provider: ProviderType, token: string): Observable<boolean> {
     return from(this.supabase.auth.updateUser({data: { [provider]: token }})).pipe(
       map(() => true),
-      catchError(() => of(false))
+      catchError((error) => {
+        this.toastService.error($localize`:@@FAILED_TO_STORE_TOKEN:Failed to store ${provider} token. Please try again later.`);
+        return of(false);
+      })
     );
   }
 
   getToken(provider: ProviderType): Observable<string | null> {
     return from(this.auth.getUser()).pipe(
       map((user) => {
-        return user?.user_metadata[provider] || null}
-      ),
-      catchError(() => of(null))
+        return user?.user_metadata[provider] || null;
+      }),
+      catchError((error) => {
+        this.toastService.error($localize`:@@FAILED_TO_GET_TOKEN:Failed to get ${provider} token. Please try again later.`);
+        return of(null);
+      })
     );
   }
 
   removeToken(provider: ProviderType): Observable<boolean> {
     return from(this.supabase.auth.updateUser({data: { [provider]: undefined }})).pipe(
       map(() => true),
-      catchError(() => of(false))
+      catchError((error) => {
+        this.toastService.error($localize`:@@FAILED_TO_REMOVE_TOKEN:Failed to remove ${provider} token. Please try again later.`);
+        return of(false);
+      })
     );
   }
 }
