7ec3c3eea7f3ea5637283ad91642313f4864c9fb
hhfrancois <francois.achache@gmail.com>
2025-04-13 11:23:37 +0200

    Integrate ToastService for error handling in RepositoryService
    - Added ToastService to RepositoryService to provide user-friendly notifications for error scenarios during repository estimation and translation.
    - Implemented toast notifications for errors when invoking API functions, enhancing user feedback during interactions.
    - Refactored error handling to replace direct error logging with toast messages for a better user experience.

--- src/app/core/services/repository.service.ts
+++ src/app/core/services/repository.service.ts
@@ -3,24 +3,31 @@ import { Observable, from, map } from 'rxjs';
 import { Job } from '../../shared/models/job.model';
 import { Repository } from '../../shared/models/repository.model';
 import { SupabaseClientService } from './supabase-client.service';
-
-
+import { ToastService } from './toast.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepositoryService {
   private supabaseClientService = inject(SupabaseClientService);
+  private toastService = inject(ToastService);
 
   estimateRepository(repository: Repository, {branch, ext, transUnitState}: {branch: string, ext: string, transUnitState: string}): Observable<Job> {
     const { namespace, name, provider } = repository;
     const request = { namespace, name, branch, ext, transUnitState };
     return from(this.supabaseClientService.functions.invoke<{message: string, job: Job}>(`estimate/${provider}`, {method: 'POST', body: request})).pipe(
-      map(response => response.data),
+      map(response => {
+        if (response.error) {
+          this.toastService.error($localize`:@@FAILED_TO_ESTIMATE_REPOSITORY:Failed to estimate repository. Please try again later.`);
+          throw new Error('Error estimating repository');
+        }
+        return response.data;
+      }),
       map(data => {
         if(data?.job) {
           return data.job;
         }
+        this.toastService.error($localize`:@@FAILED_TO_ESTIMATE_REPOSITORY:Failed to estimate repository. Please try again later.`);
         throw new Error('Error estimating repository');
       })
     );
@@ -30,11 +37,18 @@ export class RepositoryService {
     const { namespace, name, provider } = repository;
     const request = { namespace, name, branch, ext, transUnitState, procedeedTransUnitState };
     return from(this.supabaseClientService.functions.invoke<{message: string, job: Job}>(`translate/${provider}`, {method: 'POST', body: request})).pipe(
-      map(response => response.data),
+      map(response => {
+        if (response.error) {
+          this.toastService.error($localize`:@@FAILED_TO_TRANSLATE_REPOSITORY:Failed to translate repository. Please try again later.`);
+          throw new Error('Error translating repository');
+        }
+        return response.data;
+      }),
       map(data => {
         if(data?.job) {
           return data.job;
         }
+        this.toastService.error($localize`:@@FAILED_TO_TRANSLATE_REPOSITORY:Failed to translate repository. Please try again later.`);
         throw new Error('Error translating repository');
       })
     );
