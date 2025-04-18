9debd3f822080df9ca5d09be09dd9ec2d049dd46
hhfrancois <francois.achache@gmail.com>
2025-04-18 12:08:45 +0200

    Update Stripe Public Key in Production Environment
    - Replaced the test public key with the live public key for Stripe integration in the production environment file, ensuring proper payment processing in the application.

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -5,7 +5,7 @@ export const environment = {
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmcmRtaHNya2xydnRhYXRkdmJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2NjU5ODMsImV4cCI6MjA1NzI0MTk4M30.1pMgwZcAKuPC1dDOi4MA64ncu2KsIx2mnEGK30obzBk'
   },
   stripe: {
-    publicKey: 'pk_test_51R0h2VLJCrbo8n9zhGPY9DIVhwKrlLFzUQA8AhavloPHtavV2cVfoTbk1qRRD0R7SwYV5wyfE3jyCKhT9WyUAuLy00ax47FDgu',
+    publicKey: 'pk_live_51R0h2VLJCrbo8n9zlLDzmtJPvWeW9sRq4NExsNCW0Zzuhe25npbQNv5DfEtX4uoi2gVUQf6gKFFjJ9xdmwd7UwHe00eA06BK0e',
     silverPriceId: 'price_1RF87TLJCrbo8n9z2Vgs4x4l',
     goldPriceId: 'price_1RF87lLJCrbo8n9zeqgvvQKy',
     diamondPriceId: 'price_1RF881LJCrbo8n9zaar5Bcvg'
